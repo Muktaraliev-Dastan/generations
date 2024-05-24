1da880f81505ef72e22b46218dcec988699cbf95
Muktarali-uulu-Dastanbek <muktaraliuuludastan@gmail.com>
2024-05-25 01:34:44 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/generations/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.generations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      themeMode: ThemeMode.system,
+      home: Screen1(),
+    );
+  }
+}
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Generations App",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
